a336ae7920754b7fb882076eb4f4c8f4e2158179
mentalgear <tomfaber7@gmail.com>
2022-07-20 00:51:23 +0200

    !build: upgrade Vite 3 / other deps

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,18 @@
-import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-static";
+import preprocess from "svelte-preprocess"
+import adapter from "@sveltejs/adapter-static"
 //import node from "@sveltejs/adapter-node";
 
 const dev = process.env.NODE_ENV == "development"
 
 export default {
-  kit: {
+    kit: {
+        //adapter: node()
+        adapter: adapter(),
 
-    //adapter: node()
-    adapter: adapter(),
-
-    prerender: {
-        default: true
+        prerender: {
+            default: true,
+        },
     },
 
-    vite: {
-      compilerOptions: { dev },
-    }
-  },
-
-  preprocess: preprocess(),
-};
+    preprocess: preprocess(),
+}
